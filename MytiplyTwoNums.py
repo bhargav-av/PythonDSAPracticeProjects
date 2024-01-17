456fbea840cccd0dd12696b6e0942c4d4fb483dc
Bhargav AV <venkatabhargavit@gmail.com>
2024-01-17 21:44:56 +0530

    This is an Initial Commit

---
+++ MytiplyTwoNums.py
@@ -0,0 +1,10 @@
+# Write a program tht takes two integers and print the product.
+
+# Take input from keyboard
+number1 = int(input("Enter the first number :"))
+number2 = int(input("Enter the second number :"))
+
+
+# Calculate  Product
+product = number1 * number2
+print(f"{number1} * {number2} = {product}")
