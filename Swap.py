154deffc2ce486ef9d5bb482ad343cb49d2ff4d1
Bhargav AV <venkatabhargavit@gmail.com>
2024-01-18 20:44:37 +0530

    Swap the values of two variables.

---
+++ Swap.py
@@ -0,0 +1,21 @@
+# Swap the values of two variables.
+
+# Accept two integers from keyboard.
+num1 = input("Enter the first value: ")
+num2 = input("Enter the second value: ")
+
+# Before Swap
+print("Before swap")
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+
+# Swap values
+temp = num1
+num1 = num2
+num2 = temp
+
+# After Swap
+print("After swap")
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+
